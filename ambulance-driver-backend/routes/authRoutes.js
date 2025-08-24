359a907b95b1aa848d32952eaeb3057288c647bb
Shashank Gowda C <136600133+shashankgowda7892@users.noreply.github.com>
2025-08-24 12:50:53 +0530

    remove files

--- ambulance-driver-backend/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { login,logout } = require("../controllers/authController");
-const router = express.Router();
-
-// Login route
-router.post("/login", login);
-// router.get("/logout",logout)
-module.exports = router;
